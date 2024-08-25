b536438fd1a6ecb5c0563ea8d5db509865339d84
svalmaz <almaz.0.saparbaev@gmail.com>
2024-08-25 16:32:56 +0600

    Initial Commit

---
+++ MyMedic/MyMedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedicApp.swift
+//  MyMedic
+//
+//  Created by Almaz Saparbaev on 25/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMedicUITests/MyMedicUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyMedicUITestsLaunchTests.swift
+//  MyMedicUITests
+//
+//  Created by Almaz Saparbaev on 25/8/24.
+//
+
+import XCTest
+
+final class MyMedicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
